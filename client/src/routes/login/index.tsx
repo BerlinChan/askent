c9faefcdef4b9302b795d15c4ce8f4b19367fde0
Berlin Chan <berlinchancom@gmail.com>
2020-01-07 22:07:34 +0800

    login ui

--- client/src/routes/login/index.tsx
+++ client/src/routes/login/index.tsx
@@ -1,20 +1,95 @@
 import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
+import {
+  TextField,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress
+} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { useSignupMutation } from "../../generated/graphqlHooks";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    signupBox: {
+      display: "flex",
+      justifyContent: "center",
+      marginTop: theme.spacing(4)
+    },
+    form: {
+      width: 475
+    },
+    card: {
+      padding: theme.spacing(2)
+    },
+    buttonWrapper: {
+      margin: theme.spacing(1),
+      position: "relative"
+    },
+    buttonProgress: {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      marginTop: -12,
+      marginLeft: -12
+    }
+  })
+);
 
 const Login: React.FC = () => {
-  let history = useHistory();
-  let location = useLocation();
-
-  let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-      history.replace(from);
+  const classes = useStyles();
+  const [signupMutation, { data, loading, error }] = useSignupMutation();
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: "/" } };
+  const login = () => {
+    history.replace(from);
   };
 
   return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
-    </div>
+    <Box className={classes.signupBox}>
+      <form className={classes.form} noValidate autoComplete="off">
+        <Card className={classes.card}>
+          <CardContent>
+            <TextField
+              required
+              fullWidth
+              label="Email"
+              type="email"
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              margin="normal"
+            />
+          </CardContent>
+          <CardActions>
+            <div className={classes.buttonWrapper}>
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={loading}
+                onClick={login}
+              >
+                Log In
+              </Button>
+              {true && (
+                <CircularProgress
+                  size={24}
+                  className={classes.buttonProgress}
+                />
+              )}
+            </div>
+          </CardActions>
+        </Card>
+      </form>
+    </Box>
   );
 };
 
