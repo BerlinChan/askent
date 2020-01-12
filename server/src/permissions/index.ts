e52c5c97e36bc92c70010556dba39ff8ea08c8a5
Berlin Chan <berlinchancom@gmail.com>
2020-01-12 16:32:17 +0800

    debug deleteEvent

--- server/src/permissions/index.ts
+++ server/src/permissions/index.ts
@@ -1,31 +1,41 @@
-import {rule, shield} from 'graphql-shield'
-import {getUserId} from '../utils'
+import { rule, shield } from 'graphql-shield'
+import { getUserId } from '../utils'
 
 const rules = {
-    isAuthenticatedUser: rule()((parent, args, context) => {
-        const userId = getUserId(context)
-        return Boolean(userId)
-    }),
-    isQuestionAuthor: rule()(async (parent, {questionId}, context) => {
-        const userId = getUserId(context)
-        const author = await context.photon.questions.findOne({where: {id: questionId}})
-            .author()
-        return userId === author.id
-    }),
+  isAuthenticatedUser: rule()((parent, args, context) => {
+    const userId = getUserId(context)
+    return Boolean(userId)
+  }),
+  isEventAuthor: rule()(async (parent, { eventId }, context) => {
+    const userId = getUserId(context)
+    const owner = await context.photon.events
+      .findOne({ where: { id: eventId } })
+      .owner()
+      
+    return userId === owner.id
+  }),
+  isQuestionAuthor: rule()(async (parent, { questionId }, context) => {
+    const userId = getUserId(context)
+    const author = await context.photon.questions
+      .findOne({ where: { id: questionId } })
+      .author()
+
+    return userId === author.id
+  }),
 }
 
 export const permissions = shield({
-    Query: {
-        events: rules.isAuthenticatedUser,
-        questions: rules.isAuthenticatedUser,
-        checkEventCodeExist: rules.isAuthenticatedUser,
-    },
-    Mutation: {
-        createEvent: rules.isAuthenticatedUser,
-        updateEvent: rules.isQuestionAuthor,
-        deleteEvent: rules.isQuestionAuthor,
-        createQuestion: rules.isAuthenticatedUser,
-        updateQuestion: rules.isQuestionAuthor,
-        deleteQuestion: rules.isQuestionAuthor,
-    },
+  Query: {
+    events: rules.isAuthenticatedUser,
+    questions: rules.isAuthenticatedUser,
+    checkEventCodeExist: rules.isAuthenticatedUser,
+  },
+  Mutation: {
+    createEvent: rules.isAuthenticatedUser,
+    updateEvent: rules.isEventAuthor,
+    deleteEvent: rules.isEventAuthor,
+    createQuestion: rules.isAuthenticatedUser,
+    updateQuestion: rules.isQuestionAuthor,
+    deleteQuestion: rules.isQuestionAuthor,
+  },
 })
