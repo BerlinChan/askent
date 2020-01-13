03080233ede78b0a8a080206dee0e742d16751f0
Berlin Chan <berlinchancom@gmail.com>
2020-01-13 23:28:30 +0800

    tabPanel

--- client/src/routes/event/questions/index.tsx
+++ client/src/routes/event/questions/index.tsx
@@ -6,51 +6,107 @@ import {
   Box,
   Typography,
   FormControlLabel,
-  Switch
+  Switch,
+  Tabs,
+  Tab
 } from "@material-ui/core";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { FormattedMessage } from "react-intl";
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  withStyles
+} from "@material-ui/core/styles";
+import { FormattedMessage, useIntl } from "react-intl";
+import TabPanel from "../../../components/TabPanel";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    questionsGrid: { marginTop: theme.spacing(1) },
     reviewActions: {
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center",
-      flexWrap: "nowrap",  
-     },
+      flexWrap: "nowrap"
+    },
+    tabPanel: {
+      minHeight: 200,
+      display: "flex",
+      alignItems: "stretch"
+    }
   })
 );
 
+const QuestionTabs = withStyles({
+  root: {
+    minHeight: 32
+  }
+})(Tabs);
+const QuestionTab = withStyles({
+  root: {
+    minHeight: 32
+  }
+})(Tab);
+
 const Questions: React.FC = () => {
   const classes = useStyles();
+  const { formatMessage } = useIntl();
   let { id } = useParams();
   const { url, path } = useRouteMatch();
+  const [tabIndex, setTabIndex] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTabIndex(newValue);
+  };
 
   return (
     <React.Fragment>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} className={classes.questionsGrid}>
         <Grid item sm={6}>
           <Box className={classes.reviewActions}>
-            <Typography>
-              <FormattedMessage id="Moderation" defaultMessage="Moderation" />
+            <Typography color="textSecondary">
+              <FormattedMessage id="ForReview" defaultMessage="For view" />
             </Typography>
             <FormControlLabel
+              labelPlacement="start"
               control={<Switch value="checkedC" />}
-              label="Uncontrolled"
+              label={formatMessage({
+                id: "Moderation",
+                defaultMessage: "Moderation"
+              })}
             />
           </Box>
-          <Paper>
+          <li>
             <div>Event Questions</div>
             <ul>
               <li> id: {id}</li>
               <li> path: {path}</li>
               <li> url: {url}</li>
             </ul>
-          </Paper>
+          </li>
         </Grid>
         <Grid item sm={6}>
-          <Paper>xs=12</Paper>
+          <Paper>
+            <QuestionTabs value={tabIndex} onChange={handleChange}>
+              <QuestionTab
+                label={formatMessage({
+                  id: "Live",
+                  defaultMessage: "Live"
+                })}
+              />
+              <QuestionTab
+                label={formatMessage({
+                  id: "Archive",
+                  defaultMessage: "Archive"
+                })}
+              />
+            </QuestionTabs>
+            <TabPanel value={tabIndex} index={0}>
+              <Box className={classes.tabPanel}> Item One</Box>
+            </TabPanel>
+            <TabPanel value={tabIndex} index={1}>
+              <Box className={classes.tabPanel}>Item 2</Box>
+            </TabPanel>
+          </Paper>
         </Grid>
       </Grid>
     </React.Fragment>
