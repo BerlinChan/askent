df2a5794e43f41299e4c7fc126dd35451f993d1e
chenbolin <chenbolin@ygsoft.com>
2020-01-20 17:30:59 +0800

    layout home

--- client/src/routes/home/HomeHeader.tsx
+++ client/src/routes/home/HomeHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Box, Link, Toolbar } from "@material-ui/core";
+import { Box, Link, Toolbar, Container } from "@material-ui/core";
 import AppBarElevationScroll from "../../components/AppBarElevationScroll";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -18,27 +18,24 @@ export default function HomeHeader() {
 
   return (
     <AppBarElevationScroll>
-      <Toolbar className={classes.toolbar}>
-        <Link
-          color="inherit"
-          component={RouterLink}
-          to="/"
-          variant="h6"
-        >
-          Askent
-        </Link>
-        <Box className={classes.actions}>
-          <Link color="inherit" component={RouterLink} to="/admin">
-            Admin
+      <Container maxWidth="lg">
+        <Toolbar className={classes.toolbar}>
+          <Link color="inherit" component={RouterLink} to="/" variant="h6">
+            Askent
           </Link>
-          <Link color="inherit" component={RouterLink} to="/login">
-            Log In
-          </Link>
-          <Link color="inherit" component={RouterLink} to="/signup">
-            Sign Up
-          </Link>
-        </Box>
-      </Toolbar>
+          <Box className={classes.actions}>
+            <Link color="inherit" component={RouterLink} to="/admin">
+              Admin
+            </Link>
+            <Link color="inherit" component={RouterLink} to="/login">
+              Log In
+            </Link>
+            <Link color="inherit" component={RouterLink} to="/signup">
+              Sign Up
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBarElevationScroll>
   );
 }
--- client/src/routes/home/index.tsx
+++ client/src/routes/home/index.tsx
@@ -1,24 +1,87 @@
 import React from "react";
-import { TextField, InputAdornment, Button } from "@material-ui/core";
+import {
+  Grid,
+  InputAdornment,
+  Container,
+  Card,
+  CardActions,
+  CardContent
+} from "@material-ui/core";
 import HomeHeader from "./HomeHeader";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { FTextField, ButtonLoading } from "../../components/Form";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    joinForm: {
+      marginTop: theme.spacing(2)
+    },
+    card: {
+      padding: theme.spacing(2)
+    }
+  })
+);
 
 const Home: React.FC = props => {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <HomeHeader />
-      <form noValidate autoComplete="off">
-        <TextField
-          id="filled-basic"
-          label="Event Code"
-          variant="filled"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">#</InputAdornment>
-          }}
-        />
-      </form>
-      <Button variant="contained" color="primary">
-        Join
-      </Button>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item md={4}>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={Yup.object({
+                email: Yup.string()
+                  .email()
+                  .required(),
+                password: Yup.string()
+                  .max(20)
+                  .required()
+              })}
+              onSubmit={async values => {}}
+            >
+              <Form className={classes.joinForm}>
+                <Card className={classes.card}>
+                  <CardContent>
+                    <FTextField
+                      autoFucous
+                      id="eventCode"
+                      name="eventCode"
+                      label="Event Code"
+                      variant="filled"
+                      margin="normal"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">#</InputAdornment>
+                        )
+                      }}
+                    />
+                  </CardContent>
+                  <CardActions>
+                    <ButtonLoading
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      loading={false}
+                      disabled={false}
+                    >
+                      Join
+                    </ButtonLoading>
+                  </CardActions>
+                </Card>
+              </Form>
+            </Formik>
+          </Grid>
+          <Grid item md={8}>
+            Other components
+          </Grid>
+        </Grid>
+      </Container>
     </React.Fragment>
   );
 };
